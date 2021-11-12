cde4e2096f607463cfe40914636acc2bca6f9abd
cjsrhd94 <cjsrhd94@gmail.com>
2021-11-12 21:15:26 +0900

    refactor: BeforeAdvice, AfterReturning 클래스 기능 통합

--- src/main/java/com/fastcampus/biz/common/BeforeAfterAdvice.java
+++ src/main/java/com/fastcampus/biz/common/BeforeAfterAdvice.java
@@ -3,7 +3,19 @@ package com.fastcampus.biz.common;
 import com.fastcampus.biz.user.UserVO;
 import org.aspectj.lang.JoinPoint;
 
-public class AfterReturningAdvice {
+public class BeforeAfterAdvice {
+
+    // Around로 등록되는 메소드만 ProceedingJoinPoint를 받고,
+    // 나머지는 JoinPoint를 받아야 한다.
+    public void beforeLog(JoinPoint jp) {
+        String method = jp.getSignature().getName();
+
+        // JoinPoint의 getArgs() 메소드를 이용하면 비즈니스 메소드 호출 시 전달된 값을 얻어낼 수 있다.
+        Object[] args = jp.getArgs();
+
+        System.out.println("[ 사전 처리 ] " + method +
+                "() 메소드 ARGS 정보 : " + args[0].toString());
+    }
 
     public void afterLog(JoinPoint jp, Object returnObj){
         String method = jp.getSignature().getName();
